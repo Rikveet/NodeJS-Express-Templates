58483bf27510eb743b6e85cc13d3751200fb5d34
CaptKillSwitch <rh18vo@gmail.com>
2023-01-25 17:18:51 -0500

    add request with parameters

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
+// request with parameter
+app.get('/:name', (req, res)=>{
+    const {name} = req.params;
+    res.send(`Hello ${name}!!`);
+})
+
 // listen to port or default 3000
 app.listen(process.env.PORT || '3000');
 console.log('Server has started');
