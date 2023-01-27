ef860896e5446dfa689be5d5d05197afd029b86d
CaptKillSwitch <rh18vo@gmail.com>
2023-01-27 17:50:23 -0500

    Fixing build workflow

--- index.js
+++ index.js
@@ -6,14 +6,15 @@ dotenv.config()
 
 // express app init
 const express = require('express');
+const path = require("path");
 const app = express();
 
 // Configuration of static pages. This can be your React build.
-app.use('/', express.static('public'));
+app.use('/', express.static(path.join(__dirname, 'client/build')));
 
 
 // you can continue to define your api calls.
 
 // listen to port or default 3000
 app.listen(process.env.PORT || '3000');
-console.log('Server has started');
+console.log(`Server has started at https://localhost:${process.env.PORT || '3000'}`);
